d445f60022d89d752a3f561dafb77550cb8888b3
Hao Huang <haohcraft@gmail.com>
2019-10-28 08:57:55 -0400

    setup basic view

--- app/src/main/java/com/example/playwithdragger2/MainActivity.kt
+++ app/src/main/java/com/example/playwithdragger2/MainActivity.kt
@@ -2,11 +2,20 @@ package com.example.playwithdragger2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val textData = TextData()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        textView_test.text = textData.text
     }
+}
+
+
+class TextData {
+    val text = "Testing..."
 }
